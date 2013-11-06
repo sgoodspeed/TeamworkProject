29c580ceb2d9b732b88e24950b8483a1ed47599f
sgoodspeed <srgoodspeed@gmail.com>
2013-11-06 18:33:46 -0500

    First Commit
    Created a GUI button which loads an empty scene 'gameScene'

---
+++ Teamwork/Assets/UiScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI (){
+		
+		if(GUI.Button (new Rect(200f,200f,200f,200f), "Start Game")){
+			Application.LoadLevel("gameScene");	
+		}
+	}
+
+}
