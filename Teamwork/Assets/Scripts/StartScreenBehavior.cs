6ce2e93686d1f80373a842e50bcf16f3721727b3
sgoodspeed <srgoodspeed@gmail.com>
2013-11-16 16:56:31 -0500

    Created Start Screen

---
+++ Teamwork/Assets/Scripts/StartScreenBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		//Plane with an image attached is our background, if the player 
+		//clicks anywhere on screen it will activate onmousedown
+	Application.LoadLevel("GameplayScene");	
+	}
+}
